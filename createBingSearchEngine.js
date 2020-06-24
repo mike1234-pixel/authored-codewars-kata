31cea3d91967ac09f1c41a0acb249f1727ccbb1d
Michael Tandy <mike.tandy.1@gmail.com>
2020-06-24 19:20:54 +0100

    formatted code in description with Markdown, added random tests, changed conditions so output is always of type array.

--- createBingSearchEngine.js
+++ createBingSearchEngine.js
@@ -5,21 +5,21 @@
 // Create your own version of Bing's famous search algorithm.
 
 // The function will take two arguments:
-// - userInput (a string)
-// - websiteList (an array of sites)
+// - `userInput` (a string)
+// - `websiteList` (an array of sites)
 
 // Return the sites where the domain name contains the user input, as a new array.
 
-// If userInput is either undefined or an emptry string, return "No User Input."
+// If `userInput` is either `undefined` *or* an **empty string**, return `["No User Input."]`
 
-// If websiteList is undefined, return "Bing is currently offline. Please try google.com."
+// If `websiteList` is `undefined`, return `["Bing is currently offline. Please try google.com."]`
 
-// White space in the user input should not make the test fail and the function should not be case sensitive.
+// *White space in the user input should not make the test fail and the function should not be case sensitive.*
 
 // Example:
-// - userInput = "cat"
-// - websiteList = ["catphotos.com", "jumanji.com", "googlesucks.com", "catcraycray.com"];
-// - function should return ["catphotos.com", "catcraycray.com"];
+// - `userInput = "cat";`
+// - `websiteList = ["catphotos.com","jumanji.com","googlesucks.com","catcraycray.com"];`
+// - function should return `["catphotos.com", "catcraycray.com"];`
 
 // COMPLETE SOLUTION
 
@@ -28,17 +28,20 @@ searchAlgorithm = (userInput, websiteList) => {
 
   // 1) userInput empty / undefined
   if (userInput === "" || userInput === undefined) {
-    return "No User Input.";
+    searchResults.push("No User Input.");
+    return searchResults;
 
     // 2) websiteList undefined
   } else if (websiteList === undefined) {
-    return "Bing is currently offline. Please try google.com.";
+    searchResults.push("Bing is currently offline. Please try google.com.");
+    return searchResults;
   } else {
-    // remove spaces from userInput & convert to lowercase
+    // remove spaces from userInput
     if (/\s/.test(userInput)) {
-      userInput = userInput.replace(/\s/g, "").toLowerCase();
-      console.log(userInput);
+      userInput = userInput.replace(/\s/g, "");
     }
+    userInput = userInput.toLowerCase();
+    console.log(userInput); // convert to lowercase
 
     // 3) push matching search results to new array
     for (var i = 0; i < websiteList.length; i++) {
@@ -78,28 +81,27 @@ Test.assertDeepEquals(
   ]),
   ["jim.com", "jumanjim.com"]
 );
-Test.assertEquals(
+Test.assertDeepEquals(
   searchAlgorithm("", [
     "seveneleven.com",
     "jumanji.com",
     "eleventyfirstbirthday.com",
     "catcraycray.com",
   ]),
-  "No User Input."
+  ["No User Input."]
 );
-Test.assertEquals(
+Test.assertDeepEquals(
   searchAlgorithm(undefined, [
     "seveneleven.com",
     "jumanji.com",
     "eleventyfirstbirthday.com",
     "catcraycray.com",
   ]),
-  "No User Input."
+  ["No User Input."]
 );
-Test.assertEquals(
-  searchAlgorithm("dog", undefined),
-  "Bing is currently offline. Please try google.com."
-);
+Test.assertDeepEquals(searchAlgorithm("dog", undefined), [
+  "Bing is currently offline. Please try google.com.",
+]);
 Test.assertDeepEquals(
   searchAlgorithm("on", ["jon.com", "no.com", "yes.com", "gum.com"]),
   ["jon.com"]
@@ -141,3 +143,49 @@ Test.assertDeepEquals(
   ]),
   ["charliebrown.com"]
 );
+
+// RANDOM TESTS
+makeString2Chars = () => {
+  var array = [];
+  var possible = "abdefghijklnpqrstuvwxyz"; // exclude 'c', 'o', 'm' so won't match .com
+  for (var i = 0; i < 2; i++) {
+    array.push(possible[Math.floor(Math.random() * possible.length)]);
+  }
+  return array.join("");
+};
+
+makeString20Chars = () => {
+  var array = [];
+  var possible = "abdefghijklnpqrstuvwxyz";
+  for (var i = 0; i < 20; i++) {
+    array.push(possible[Math.floor(Math.random() * possible.length)]);
+  }
+  return array.join("");
+};
+
+// run random test
+randomTest = () => {
+  const randomString = makeString2Chars();
+  const websitesList = [];
+
+  for (var i = 0; i < 100; i++) {
+    websitesList.push(makeString20Chars() + ".com");
+  }
+
+  let expectedResult = [];
+
+  for (var i = 0; i < websitesList.length; i++) {
+    if (websitesList[i].includes(randomString)) {
+      expectedResult.push(websitesList[i]);
+    }
+  }
+
+  Test.assertDeepEquals(
+    searchAlgorithm(randomString, websitesList),
+    expectedResult
+  );
+};
+
+for (var i = 0; i < 100; i++) {
+  randomTest();
+}
